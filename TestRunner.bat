39e3100f8ad228ca7485004390c63d953eadac32
AjayKumar <ajaykr.mailbox@gmail.com>
2021-03-10 20:13:40 +0530

    RunnerConfig.properties add

--- TestRunner.bat
+++ TestRunner.bat
@@ -1,39 +1,39 @@
-@echo off
-echo provide the details here:
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+CALL :ConfigRun
 
-echo Select environment
-echo 1. UAT
-echo 2. SIT
-echo 3. STG
-::set choice=
-set /p choice=Enter the number to select environment:
-if '%choice%'=='1' set environment=UAT
-if '%choice%'=='2' set environment=SIT
-if '%choice%'=='3' set environment=STG
+:ConfigRun
+cls
+CALL :GetPathForConfig
+if exist %pathForConfig% (
 
-echo Select TechStack
-echo 1. LOCAL CHROME
-echo 2. LOCAL FIREFOX
-echo 3. LOCAL IEXPLORER
-echo 4. REMOTE HTMLUNIT CHROME
-echo 5. REMOTE HTMLUNIT FIREFOX
-echo 6. REMOTE HTMLUNIT IEXPLORER
-::set choice=
-set /p choice=Enter the number to select TechStack:
-if '%choice%'=='1' set teckStack=LOCAL_CHROME
-if '%choice%'=='2' set teckStack=LOCAL_FIREFOX
-if '%choice%'=='3' set teckStack=LOCAL_IEXPLORER
-if '%choice%'=='4' set teckStack=REMOTE_HTMLUNIT_CHROME
-if '%choice%'=='5' set teckStack=REMOTE_HTMLUNIT_FIREFOX
-if '%choice%'=='6' set teckStack=REMOTE_HTMLUNIT_IEXPLORER
+For /F "tokens=1* delims==" %%i IN (%pathForConfig%) DO (
+    echo [%%i]
+    pause
+    if [%%i]==[test] (
+        set cmd%%iOption=-D%%i=%%j
+    ) else (
+        set cmd%%iOption=-Dcukes.%%i=%%j
+    )
+)
+) else (
+    echo "file doesn't exist"
+)
+CALL :FinalStep
+EXIT /B 0
 
-echo Select Test Suite
-echo 1. Smoke Suite
-echo 2. Regression Suite
+:FinalStep
+set finalRunCmd=mvn clean install %cmdtestOption% %cmdenvironmentOption% %cmdtechStackOption% -Dorg.apache.logging.log4j.level=DEBUG -Dcukes.selenium.defaultFindRetries=2
+cls
+echo %finalRunCmd%
+CALL %finalRunCmd%
+PAUSE
+EXIT
 
-::set choice=
-set /p choice=Enter the number to select Test Suite:
-if '%choice%'=='1' set suiteXmlFile=SmokeSuite
-if '%choice%'=='2' set suiteXmlFile=RegressionSuite
+:GetPathForConfig
+set fileSeperator=\
+set workingDir=%CD%
+set pathForConfig=%workingDir%%fileSeperator%RunnerConfig.properties
+EXIT /B 0
 
-mvn clean test -Denvironment=%environment% -DtechStack=%teckStack% -DsuiteXmlFile=%suiteXmlFile%
+endLocal
